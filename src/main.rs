7c7b26cc2a505716f2e115714715f23550e0dd47
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2019-01-06 00:12:51 -0500

    very bad security, but PoC for listing and printing files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate docopt;
-
 use docopt::Docopt;
 
 extern crate serde_json;
-
 use serde_json::{Value, Error};
 
 extern crate socket2;
 
 extern crate hostname;
 
+extern crate walkdir;
+use walkdir::WalkDir;
+
+extern crate number_prefix;
+use number_prefix::{decimal_prefix, Standalone, Prefixed};
+
 use std::io::prelude::*;
 use std::io;
-use std::fs::File;
+use std::fs;
+use std::fs::{File,DirEntry};
+use std::path::Path;
+use std::fmt::Write;
 use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
 
 use std::time::Duration;
@@ -27,7 +34,7 @@ A networked filesystem operating over multicast.
 The goal is to make sharing of files over a LAN as simple and trivial as possible.
 
 Usage:
-  mcastfs client [--group4=<group4>] [--port=<port>] <args>...
+  mcastfs client [--group4=<group4>] [--port=<port>] [--ctimeout=<ctimeout>] <args>...
   mcastfs serve  [--group4=<group4>] [--port=<port>] [--hostname=<hostname>] <args>...
   mcastfs (-h | --help)
   mcastfs --version
@@ -38,6 +45,7 @@ Options:
   --group4=<group4>      Multicast IP [default: 224.0.21.12].
   --port=<port>          Multicast Port [default: 2112].
   --hostname=<hostname>  Hostname override.
+  --ctimeout=<ctimeout>  Client receive timeout ms [default: 400]
 "#;
 
 #[derive(Debug, Deserialize)]
@@ -46,6 +54,7 @@ struct Args {
     flag_group4: String,              // "239.1.0.1"
     flag_port: u16,                   // 2112
     flag_hostname: Option<String>,
+    flag_ctimeout: u64,
     flag_version: bool,
     cmd_client: bool,
     cmd_serve: bool,
@@ -87,23 +96,27 @@ fn sanitize_args(args: &Args) {
 
 fn do_client(args: &Args) {
   let socket = UdpSocket::bind("0.0.0.0:2121").expect("couldn't bind to 0.0.0.0:2121");
-  socket.set_read_timeout(Some(Duration::from_millis(1200))).expect("Could not set timeout to 1200ms");
+  socket.set_read_timeout(Some(Duration::from_millis(args.flag_ctimeout))).expect("Could not set timeout to 1200ms");
   
   let json_str = serde_json::to_string(&args.arg_args).expect("Could not go from Vec<String> to JSON");
   
   socket.send_to(&json_str.as_bytes(), format!("{}:{}", args.flag_group4, args.flag_port)).expect("");
   
   let mut buf = [0u8; 8 * 1024];
-  match socket.recv_from(&mut buf) {
-    Ok((len, _remote_addr)) => {
-      let data = &buf[..len];
-      let data = String::from_utf8_lossy(data);
-      
-      println!("Got back from server: {}", data);
-      
-    }
-    Err(err) => {
-      println!("Client got an error: {}", err);
+  // Print received data until timeout occurs
+  loop {
+    match socket.recv_from(&mut buf) {
+      Ok((len, _remote_addr)) => {
+        let data = &buf[..len];
+        let data = String::from_utf8_lossy(data);
+        
+        println!("{}", data);
+        
+      }
+      Err(_err) => {
+        //println!("Client got an error: {}", err);
+        break;
+      }
     }
   }
   
@@ -120,6 +133,8 @@ fn do_serve(args: &Args) {
   let ipv4_socket = SocketAddrV4::new(ipv4_addr, args.flag_port);
   
   socket.bind(&SockAddr::from(ipv4_socket)).expect("Could not bind");
+  
+  println!("Serving on mcast {} port {}: {:?}", args.flag_group4, args.flag_port, args.arg_args);
   
   let mut buf = [0u8; 1024 * 8]; // receive buffer
   
@@ -153,17 +168,114 @@ fn perform_command(args: &Args, client_args: &Vec<String>, remote_addr: &SockAdd
   
   let socket = UdpSocket::bind("0.0.0.0:2122").expect("couldn't bind to 0.0.0.0:2122");
   
+  let mut response: String = String::new();
+  
+  write!(response, "From host {}:\n", args.flag_hostname.clone().unwrap_or("ERR".to_string()) ).unwrap();
   
   match client_args[0].as_str() {
     "ls" => {
-      socket.send_to("Some data".as_bytes(), remote_addr.as_inet().expect("Not an ipv4 client") ).expect("");
+      for local_path in args.arg_args.clone() {
+        if Path::new(&local_path).is_dir() {
+          
+          for entry in WalkDir::new(&local_path) {
+            match entry {
+              Ok(entry) => {
+                write_file_to_response(&mut response, &entry.path().to_str().unwrap().to_string() );
+                //write!(response, "{:?}\n", entry.path().to_string_lossy() ).unwrap();
+              }
+              Err(e) => {
+                println!("{}", e);
+              }
+            }
+          }
+          
+        }
+        else {
+          write_file_to_response(&mut response, &local_path);
+        }
+      }
       
+    }
+    
+    "cat" => {
+      if client_args.len() < 2 {
+        write!(response, "Err: \"cat\" command requires at least one argument to print out.\n").unwrap();
+      }
+      else {
+        for i in 1..client_args.len() {
+          let file = &client_args[i];
+          if file_is_under_trusted_dir(&file, &args.arg_args.clone()) {
+            // Assumes ASCII files
+            let contents = fs::read_to_string(&file).unwrap();
+            write!(response, "{}\n", contents).unwrap();
+          }
+        }
+      }
     }
     
     _unk => {
       println!("unknown client_args={:?}", client_args);
+      write!(response, "unknown client_args={:?}", client_args).unwrap();
     }
   }
   
+  
+  let entire_resp = response.as_bytes();
+  let mut chunks = 1;
+  
+  'outer: loop {
+    let single_packet_size = entire_resp.len() / chunks;
+    
+    for chunk_i in 0..chunks {
+      let begin_byte = chunk_i * single_packet_size;
+      let end_byte = begin_byte + single_packet_size;
+      
+      match socket.send_to(&entire_resp[begin_byte..end_byte], remote_addr.as_inet().expect("remote_addr not an ipv4 client") ) {
+        Ok(_) => {
+          
+        }
+        Err(_e) => {
+          // Could not transmit, double number of chunks and re-try
+          chunks *= 2;
+          continue 'outer;
+        }
+      }
+      
+      // All packets sent OK
+      break 'outer;
+      
+    }
+  }
 }
 
+fn write_file_to_response(response: &mut String, filepath: &String) {
+  match fs::metadata(filepath) {
+    Ok(metadata) => {
+      match decimal_prefix(metadata.len() as f32) {
+        Standalone(bytes) => {
+          write!(response, "{}B - \"{}\"\n", bytes, filepath ).unwrap();
+        }
+        Prefixed(prefix, n) => {
+          write!(response, "{:.1} {}B - \"{}\"\n", n, prefix, filepath ).unwrap();
+        }
+      }
+    }
+    Err(e) => {
+      println!("{}", e);
+    }
+  }
+}
+
+fn file_is_under_trusted_dir(untrusted_file: &String, trusted_dirs: &Vec<String>) -> bool {
+  if ! Path::new(untrusted_file).exists() {
+    return false;
+  }
+  
+  for dir in trusted_dirs {
+    if untrusted_file.starts_with(dir) {
+      return true;
+    }
+  }
+  
+  return false;
+}
